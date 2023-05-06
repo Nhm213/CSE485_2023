53e040eb6fb6b4df842e506da384388d950dbd32
maingocduy <2051063514@e.tlu.edu.vn>
2023-05-06 17:12:11 +0700

    a test

---
+++ bai4.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="all.css">
+</head>
+<body >
+
+    <form action="bai4.php" method="post" >
+        Id: <input type="text"  name="id">
+        Name:<input type="text" name="name" >
+        Age: <input type="text" name="age">
+        Grade:<input type="text" name="grade">
+        <button type="submit" style="width:100%"></button>
+</form>
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST'){
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+        $grade = $_POST['grade'];
+    }
+    ?>  
+   <?php
+
+$handle = fopen('data.txt', 'r');
+$table = '<table>';
+while (!feof($handle)) {
+  
+    $line = fgets($handle);
+
+    $data = explode(',', $line);
+
+
+    $table .= '<tr>';
+    foreach ($data as $cell) {
+        $table .= '<td>' . $cell . '</td>';
+    }
+    $table .= '</tr>';
+}
+fclose($handle);
+
+$table .= '</table>';
+
+echo $table;
+?>
+</body>
+</html>
